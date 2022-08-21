935d399d48384093339aec78593e9dc74fbce7ca
Jody Zhou <jody625326@gmail.com>
2022-08-21 02:32:55 -0400

    fixed error on apostrophe

--- pages/404.js
+++ pages/404.js
@@ -4,12 +4,12 @@ import router from 'next/router';
 export default function NotFound() {
   return (
     <Box textAlign="center" margin={'auto'} py={10} px={6}>
-      <img style={{margin:'auto', maxHeight:'1200px'}} src='pageNotFound.svg' alt='404'></img>
+      <img style={{margin:'auto', maxHeight:'1200px'}} src='pageNotFound.svg' alt='404'/>
       <Text fontSize="18px" mt={3} mb={2}>
         Page Not Found
       </Text>
       <Text color={'gray.500'} mb={6}>
-        Ooooops... The page you're looking for does not seem to exist
+        Ooooops... The page your were looking for does not seem to exist
       </Text>
 
       <Button
